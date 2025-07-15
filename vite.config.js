ca9fab3d43a94c1b75a4a27cdd3a38bc354f6a00
Merveille Senga <msapaola@gmail.com>
2025-07-16 00:44:58 +0100

    update website new

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,20 @@ export default defineConfig({
         laravel({
             input: 'resources/js/app.jsx',
             refresh: true,
+            buildDirectory: 'build', // Spécifier le dossier de build
         }),
         react(),
     ],
+    build: {
+        outDir: 'public/build', // Définir le dossier de sortie
+        assetsDir: 'assets', // Définir le dossier des assets
+        manifest: true, // Générer le manifest
+        rollupOptions: {
+            output: {
+                assetFileNames: 'assets/[name]-[hash][extname]',
+                chunkFileNames: 'assets/[name]-[hash].js',
+                entryFileNames: 'assets/[name]-[hash].js',
+            },
+        },
+    },
 });
