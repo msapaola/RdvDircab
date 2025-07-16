ffe69691296292d520fe2b47927c4984195ed57f
Merveille Senga <msapaola@gmail.com>
2025-07-16 02:16:29 +0100

    update website new

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,26 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import react from '@vitejs/plugin-react';
+            import { defineConfig } from 'vite';
+            import laravel from 'laravel-vite-plugin';
+            import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: 'resources/js/app.jsx',
-            refresh: true,
-        }),
-        react(),
-    ],
-    base: '/build/', // Spécifier le chemin de base
-    build: {
-        outDir: 'public/build',
-        assetsDir: 'assets',
-        manifest: true,
-        rollupOptions: {
-            output: {
-                assetFileNames: 'assets/[name]-[hash][extname]',
-                chunkFileNames: 'assets/[name]-[hash].js',
-                entryFileNames: 'assets/[name]-[hash].js',
-            },
-        },
-    },
-});
+            export default defineConfig({
+                plugins: [
+                    laravel({
+                        input: 'resources/js/app.jsx',
+                        refresh: true,
+                    }),
+                    react(),
+                ],
+                base: '/build/',
+                build: {
+                    outDir: 'public/build',
+                    assetsDir: 'assets',
+                    manifest: true,
+                    rollupOptions: {
+                        output: {
+                            assetFileNames: 'assets/[name]-[hash][extname]',
+                            chunkFileNames: 'assets/[name]-[hash].js',
+                            entryFileNames: 'assets/[name]-[hash].js',
+                        },
+                    },
+                },
+            });
