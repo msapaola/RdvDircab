b79d967a37d9460c7ec115b83e327f1bd436124a
Merveille Senga <msapaola@gmail.com>
2025-07-22 17:05:56 +0100

    add feautures in website for Users

--- app/Http/Middleware/CheckActive.php
+++ app/Http/Middleware/CheckActive.php
@@ -10,6 +10,11 @@ class CheckActive
 {
     public function handle(Request $request, Closure $next)
     {
+        \Log::info('CheckActive middleware exécuté', [
+            'user_id' => optional(Auth::user())->id,
+            'is_active' => optional(Auth::user())->is_active,
+            'route' => $request->path(),
+        ]);
         $user = Auth::user();
         if ($user && !$user->is_active) {
             Auth::logout();
