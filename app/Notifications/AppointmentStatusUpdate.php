e27e2a0e7831db20a632290a9134e809a00cdb2b
Merveille Senga <msapaola@gmail.com>
2025-07-27 15:16:25 +0100

    add feautures in website for Users

--- app/Notifications/AppointmentStatusUpdate.php
+++ app/Notifications/AppointmentStatusUpdate.php
@@ -4,11 +4,10 @@ namespace App\Notifications;
 
 use App\Models\Appointment;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class AppointmentStatusUpdate extends Notification implements ShouldQueue
+class AppointmentStatusUpdate extends Notification
 {
     use Queueable;
 
